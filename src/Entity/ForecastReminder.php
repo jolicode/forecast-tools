08ce318a651de6da593ec260a8c3b24de66408c1
Xavier Lacot <xavier@lacot.org>
2020-04-15 12:12:37 +0200

    add a cron expression validation in the reminder form

--- src/Entity/ForecastReminder.php
+++ src/Entity/ForecastReminder.php
@@ -11,6 +11,7 @@
 
 namespace App\Entity;
 
+use App\Validator\Constraints as AppAssert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -30,6 +31,7 @@ class ForecastReminder
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @AppAssert\CronExpression(message="The value ""{{ value }}"" is not a valid cron expression.")
      */
     private $cronExpression = '0 18 * * *';
 
