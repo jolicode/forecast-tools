797b3e7a6327ad382a17f63bf58e29f00b248dd8
Xavier Lacot <xavier@lacot.org>
2020-07-03 21:11:47 +0200

    cs fixes

--- src/Migrations/Version20200703185931.php
+++ src/Migrations/Version20200703185931.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of JoliCode's Forecast Tools project.
+ *
+ * (c) JoliCode <coucou@jolicode.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,18 +21,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200703185931 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE public_forecast ADD placeholders LONGTEXT DEFAULT "a:0:{}" COMMENT \'(DC2Type:array)\'');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE public_forecast DROP placeholders');
