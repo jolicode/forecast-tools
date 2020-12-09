644c423e056dad266f3a17708d904a0f06282c32
Xavier Lacot <xavier@lacot.org>
2020-12-09 10:50:41 +0100

    remove web server bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of JoliCode's Forecast Tools project.
- *
- * (c) JoliCode <coucou@jolicode.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
