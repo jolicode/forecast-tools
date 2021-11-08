06d6acb6043cc17e0f8f5db0a50016831dec170c
Xavier Lacot <xavier@lacot.org>
2021-11-08 17:15:45 +0100

    CS fixes

--- src/Forecast/UniqueTokenGenerator.php
+++ src/Forecast/UniqueTokenGenerator.php
@@ -26,7 +26,7 @@ class UniqueTokenGenerator
     {
         $value = bin2hex(random_bytes(25));
 
-        if ($this->repository->findOneByToken($value) !== null) {
+        if (null !== $this->repository->findOneByToken($value)) {
             return $this->generate();
         }
 
