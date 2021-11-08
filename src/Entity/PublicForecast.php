4eb91180d1314f610f0ea0c78a4cfa81301d4fda
Xavier Lacot <xavier@lacot.org>
2021-11-08 17:22:33 +0100

    do not change the token property format, as we reverted back to a case-insensitive string

--- src/Entity/PublicForecast.php
+++ src/Entity/PublicForecast.php
@@ -33,7 +33,7 @@ class PublicForecast
     private $forecastAccount;
 
     /**
-     * @ORM\Column(type="binary", length=255)
+     * @ORM\Column(type="string", length=255)
      */
     private $token;
 
@@ -91,22 +91,9 @@ class PublicForecast
         return $this;
     }
 
-    public function getToken()
+    public function getToken(): ?string
     {
         return $this->token;
-    }
-
-    public function getTokenValue(): ?string
-    {
-        if (\is_resource($this->token)) {
-            rewind($this->token);
-
-            return stream_get_contents($this->token);
-        } elseif (\is_string($this->token)) {
-            return $this->token;
-        }
-
-        return null;
     }
 
     public function setToken(string $token): self
