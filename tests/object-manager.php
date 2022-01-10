1a72e3c183bc3240b5f07ffdbf3694a6da43590c
Xavier Lacot <xavier@lacot.org>
2022-01-10 15:39:53 +0100

    cs fixes

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -1,5 +1,13 @@
 <?php
-// tests/object-manager.php
+
+/*
+ * This file is part of JoliCode's Forecast Tools project.
+ *
+ * (c) JoliCode <coucou@jolicode.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
@@ -10,4 +18,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
+
 return $kernel->getContainer()->get('doctrine')->getManager();
